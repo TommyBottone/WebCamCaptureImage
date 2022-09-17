90b6e600c4f7281eaafc55ed451b4cdc9cec6d58
TommyBottone <thomas.bottone@gmail.com>
2022-09-17 10:04:43 -0400

    add parameters
    add parameters

--- src/WebCam.java
+++ src/WebCam.java
@@ -5,11 +5,15 @@ import static org.bytedeco.javacpp.opencv_core.IplImage;
 import static org.bytedeco.javacpp.opencv_core.cvFlip;
 import static org.bytedeco.javacpp.opencv_imgcodecs.cvSaveImage;
 
+import java.io.File;
+
 public class WebCam implements Runnable 
 {
 	final int INTERVAL = 1000;
 	CanvasFrame canvas  = new CanvasFrame("Web Cam");
 	public boolean runWebCam = true;
+	public static String file;
+	
 	public WebCam()
 	{
 	  canvas.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
@@ -53,7 +57,7 @@ public class WebCam implements Runnable
     		    {
     		      i = 0;
     		    }
-    		    cvSaveImage("D:\\pictures\\sampleImages\\" + (i++) + "-aa.jpg", img);
+    		    cvSaveImage(file + "\\img-" + (i++) + ".jpg", img);
     		    canvas.showImage(converter.convert(img));
 	        }
 	        catch(Exception e)
@@ -70,8 +74,24 @@ public class WebCam implements Runnable
     		}
 		  }
 	}
-	public static void main(String[] args)
-  {
+	
+	public static void main(String[] args){
+	if(args.length == 2) {
+		if(args[0].compareToIgnoreCase("-f") == 0) {
+			file = args[1];
+			File f = new File(file);
+			if(!f.isDirectory()){
+				System.out.println(f + " is not a file! Exiting");
+			}
+			else {
+				System.out.println(f + " found");
+			}
+		}
+		else {
+			System.out.println("Pass in parameters -f DIRECTORY_PATH");
+			return;
+		}
+	}
     WebCam webcam = new WebCam();
     Thread thread = new Thread(webcam);
     thread.start();
